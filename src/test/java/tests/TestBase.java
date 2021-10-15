b8b3b4ace1a18796f3f2cf915d1b8d472868565b
poseidonchik <muha.rox@gmail.com>
2021-10-15 15:34:44 +0300

    Добавлен селеноид и тир даун

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,27 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
     static void configBrowser(){
         Configuration.startMaximized = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
     }
 }
